104fc552c25a72dfcd6d5cdbfe142d03638e3b40
Nicolás Hüg <nhug@smartbot.uy>
2023-02-24 22:29:06 -0300

    Caso 3: Evaluar que se esté recuperando la clave favorita existente (Rojo)

--- tests/test_clavefavorita.py
+++ tests/test_clavefavorita.py
@@ -1,11 +1,23 @@
 import unittest
 
 from src.logica.Logica import Logica
+from src.modelo.clave_favorita import ClaveFavorita
+from src.modelo.declarative_base import Session
 
 class ClaveFavoritaTestCase(unittest.TestCase):
   def setUp(self):
+    self.session = Session()
     self.logica = Logica()
   
   def test_clavefavorita_01(self):
     claves = self.logica.dar_claves_favoritas()
     self.assertIsNot(claves, None)
+
+  def test_clavefavorita_02(self):
+    nueva_clave = ClaveFavorita(nombre="Clave de prueba", clave="clave 0", pista="pista 0")
+    self.session.add(nueva_clave)
+    self.session.commit()
+    self.session.close()
+    claves = self.logica.dar_claves_favoritas()
+
+    self.assertEquals(claves[0]['nombre'], "Clave de prueba")
